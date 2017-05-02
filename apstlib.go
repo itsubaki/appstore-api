812cc4f4a336ebf922346e9aacd69bc77411a52d
tsubaki <tsubaki@moon>
2017-05-02 19:14:34 +0900

    Remove root path

--- apstlib.go
+++ apstlib.go
@@ -1,7 +1,6 @@
 package apstlib
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/itsubaki/apstlib/ranking"
@@ -9,8 +8,6 @@ import (
 )
 
 func AppEngine() {
-	http.HandleFunc("/", root)
-
 	http.HandleFunc("/ranking", ranking.Latest)
 	http.HandleFunc("/ranking/capture", ranking.Capture)
 	http.HandleFunc("/ranking/search", ranking.Search)
@@ -18,8 +15,4 @@ func AppEngine() {
 	http.HandleFunc("/review", review.List)
 	http.HandleFunc("/review/capture", review.Capture)
 	http.HandleFunc("/review/search", review.Search)
-}
-
-func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ok")
 }
